0a1cb549e7ce66e877d5856f64082cdd38748086
chriye <84696540+chriye@users.noreply.github.com>
2022-01-30 19:27:26 -0800

    edit app file

--- app/app.py
+++ app/app.py
@@ -2,6 +2,11 @@ import sqlite3
 
 def get_auth_db():
     """
+    input  : nothing
+    output : a database with a message table
+    
+    Build a connection to the database. And build a table messages 
+    with columns(id, handle, and message)
     """
     # check if message_db in g attribute
     if 'message_db' not in g:
@@ -11,13 +16,40 @@ def get_auth_db():
     cmd = \
             """
             CREATE TABLE messages(
-                id INTEGER,
+                id INTEGER UNIQUE,
                 handle TEXT,
                 message TEXT);
             """
     cursor = g.message_db.cursor()
-    cursor.excute(cmd)
+    cursor.execute(cmd)
+    cursor.closs()
     return g.message_db
 
 def insert_message(request):
-    
+    """
+    input  : a request form that has user input information
+    output : nothing
+
+    Add a new row of data into the messages table. Each row contains
+    id, handle, and message. 
+    id      : the current row number of table add one.
+    handle  : the handle given by user
+    message : the message given by user
+    """
+    # connect the database
+    g.message_db = sqlite3.connect('messages_db.sqlite')
+    cursor = g.message_db.cursor()
+    # get the current row number in table messages
+    cursor.execute("SELECT * FROM messages;")
+    cur_row = cursor.fetchall() #current row num
+    # modify id
+    id = cur_row + 1
+    # insert a new row into message table
+    cmd = \
+        """
+        INSERT INTO message
+        VALUES ({0}, {1}, {2});
+        """.format(id, request.form["handle"], request.form["message"])
+    cursor.execute(cmd)
+    g.message_db.commit()
+    g.message_db.close()
