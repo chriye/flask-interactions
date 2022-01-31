0114a20647a2c166eccb0fe8147f941ae990aec2
chriye <84696540+chriye@users.noreply.github.com>
2022-01-31 01:22:44 -0800

    empty app1 and add it to init.

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,18 @@
 
 
 from flask import Flask, g, render_template, request
-
+import sqlite3
 #execfile("/PIC16B/flask-interactions/app/app.py")
 # import os
-# os.system("/PIC16B/flask-interactions/app/app.py")
+# os.system("/PIC16B/flask-interactions/app/app1.py")
+
+#from app1 import insert_message
+
+
+# import sys
+# sys.path.insert(0, '/Desktop/PIC16B/flask-interactions')
+# from app1 import insert_message
+
 
 import sklearn as sk
 import matplotlib.pyplot as plt
@@ -85,6 +93,63 @@ def view():
 #         except:
 #             return render_template('submit-basic.html', error=True)
 
+
+def get_auth_db():
+    """
+    input  : nothing
+    output : a database with a message table
+    
+    Build a connection to the database. And build a table messages 
+    with columns(id, handle, and message)
+    """
+    # check if message_db in g attribute
+    if 'message_db' not in g:
+        g.message_db = sqlite3.connect('messages_db.sqlite')
+
+    # check if messages exists in message_db
+    cmd = \
+            """
+            CREATE TABLE IF NOT EXISTS messages(
+                id INTEGER,
+                handle TEXT,
+                message TEXT);
+            """
+    cursor = g.message_db.cursor()
+    cursor.execute(cmd)
+    return g.message_db
+
+def insert_message(request):
+    """
+    input  : a request form that has user input information
+    output : nothing
+
+    Add a new row of data into the messages table by SQL command. 
+    Each row contains id, handle, and message. 
+    id      : the current row number of table add one.
+    handle  : the handle given by user
+    message : the message given by user
+    """
+
+
+    # connect the database
+    g.message_db = get_auth_db()
+    cursor = g.message_db.cursor()
+    # get the current row number in table messages
+    new_cursor = cursor.execute("SELECT * FROM messages;")
+    cur_row = new_cursor.fetchall() #current row num
+    # modify id
+    id = len(cur_row) + 1
+    # insert a new row into message table
+    cmd = \
+        """
+        INSERT INTO messages (id, handle, message)
+        VALUES ({0}, '{1}', '{2}');
+        """.format(id, request.form["handle"], request.form["message"])
+    cursor.execute(cmd)
+    # run commit to ensure a new row has been saved into messages
+    g.message_db.commit()
+    g.message_db.close()
+
 # nontrivial version: makes a prediction and shows a viz
 @app.route('/submit-advanced/', methods=['POST', 'GET'])
 def submit():
@@ -92,7 +157,14 @@ def submit():
         return render_template('submit.html')
     else:
         try:
-            # insert_message(request)
-            return render_template('submit.html', handle = request.form['handle'], message = request.form['message'])
+            insert_message(request)
+            return render_template('submit.html', thanks = True)
         except:
             return render_template('submit.html', error = True)
+
+
+
+# def random_messages(n):
+#     """
+
+#     """
